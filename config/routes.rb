37c295b9e241d5bd2b883fb6e36597c36463dcff
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-30 00:20:09 -0400

    created delete feature

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get 'pictures/:id' => 'pictures#show'
   get 'pictures/:id/edit' => "pictures#edit"
   patch 'pictures/:id' => "pictures#update"
+
+  delete 'pictures/:id' => 'pictures#destroy'
+
 end
