ca59fa7472b3ae6a2b359b36969c8671683f6160
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-10 17:46:44 -0400

    got submit button to save and redirect to root_url for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @User = Picture.new
+    @user = User.new
+
+      @user.email = params[:user][:email]
+      @user.password = params[:user][:password]
+      @user.password_confirmation = params[:user][:password_confirmation]
 
 
 
-     if @userr.save
+     if @user.save
        # if the picture gets saved, generate a get request to "/pictures" (the index)
-       redirect_to "/pictures"
+       redirect_to root_url
      else
        # otherwise render new.html.erb
        render :new
